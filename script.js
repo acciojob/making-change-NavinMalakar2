92aeba8070d4e35181e6da9192fbf521711df994
Navin Malakar <161839292+NavinMalakar2@users.noreply.github.com>
2025-07-14 09:59:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const makeChange = (c) => {
   // your name here
+	const change = {};
+change.q = Math.floor(c/ 25);
+  c %= 25;
+
+  change.d = Math.floor(c / 10);
+  c %= 10;
+
+  change.n = Math.floor(c / 5);
+  c %= 5;
+
+  change.p = c; // whatever is left are pennies
+
+  return change;
 };
 
 // Do not the change the code below
